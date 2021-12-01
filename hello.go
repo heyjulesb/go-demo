d0ab90ec07ea364d231137828cc61ce0876035bc
Jules <67839718+heyjulesb@users.noreply.github.com>
2021-12-01 16:17:30 +0000

    refactoring code for testing

--- hello.go
+++ hello.go
@@ -19,16 +19,29 @@ import "fmt"
 // 	fmt.Println(Hello())
 // }
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // the function accepts any argument of type string
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 			name = "World"
 	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name // using the name argument and concatenating it with Hello,
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
