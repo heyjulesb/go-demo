836c428f6b0c8c5750f5650f285176d0460b3d28
Jules <67839718+heyjulesb@users.noreply.github.com>
2021-12-01 11:27:59 +0000

    setting up a go file

---
+++ hello.go
@@ -0,0 +1,20 @@
+// Writing a program in Go, you will have a main package defined with a main func inside it.
+// Packages are ways of grouping up related Go code together
+package main
+
+// importing a package which contains the Println function that we use to print
+import "fmt"
+
+// // func keyword is how we define a function with a name and a body
+// func main() {
+//     fmt.Println("Hello, world")
+// }
+
+// new function but added a keyword string in the definition. This means the function returns a string.
+func Hello() string {
+	return "Hello, World"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
