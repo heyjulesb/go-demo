5fdfb68a73d64e8dc5245299cb469ab8d4b3a725
Jules <67839718+heyjulesb@users.noreply.github.com>
2021-12-01 13:51:15 +0000

    refactoring test

--- hello_test.go
+++ hello_test.go
@@ -1,18 +1,6 @@
 // To run test you need to type in go test in the terminal.
 // However before you can go that you would need to have a module file.
 // you will need to run go mod init SOMENAME in each new folder before running commands like go test or go build
-package main
-
-import "testing"
-
-func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-}
 
 // Writing tests in Go
 
@@ -21,3 +9,26 @@ func TestHello(t *testing.T) {
 // The test function takes one argument only t *testing.T
 // In order to use the *testing.T type you need to import "testing"
 // the t of type *testing.T is your "hook" in the testing framework, so you can do things like t.Fail() when you want to fail.
+
+package main
+
+import "testing"
+
+// func TestHello(t *testing.T) {
+// 	got := Hello()
+// 	want := "Hello, world"
+
+// 		if got != want {
+// 			// calling the Errorf method on our t which will print out a message and fail the test. 
+// 			// The f stands for format which allows us to build a string with values inserted into the placeholder values "%q."
+// 			t.Errorf("got %q want %q", got, want)
+// 		}
+// }
+
+func TestHello(t *testing.T) {
+	got := Hello("Chris")
+	want := "Hello, Chris"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+}
