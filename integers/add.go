d6c8081a9a748a6d26a35f5b04ca4b54c33f2834
Jules <67839718+heyjulesb@users.noreply.github.com>
2021-12-02 11:29:22 +0000

    integer files for Go

---
+++ integers/add.go
@@ -0,0 +1,7 @@
+package integers
+
+// if you have an argument of the same type instead of writing it as (x int, y int) 
+// you can shorten it to (x, y int)
+func Add(x, y int) int {
+	return x + y
+}
